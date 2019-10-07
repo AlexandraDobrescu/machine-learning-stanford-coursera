1f7c0a5a65bec21dcfebf594a868c28748f53f72
AlexandraDobrescu <alexandra.dobrescu@asaff.ro>
2019-10-07 18:50:39 +0300

    NN-Backpropagation hidden layer

---
+++ add_script.sh
@@ -0,0 +1,25 @@
+mkdir machine-learning-ex4
+cp /c/Users/cool_/Desktop/machine-learning-ex4/machine-learning-ex4/ex4/* machine-learning-ex4/
+git add .
+git commit -m "NN-Backpropagation hidden layer"
+
+mkdir machine-learning-ex5
+cp /c/Users/cool_/Desktop/machine-learning-ex5/machine-learning-ex5/ex5/* machine-learning-ex5/
+git add .
+git commit -m "Regularized LR-Bias-Variance-Polynomial regression"
+
+mkdir machine-learning-ex6
+cp /c/Users/cool_/Desktop/machine-learning-ex6/machine-learning-ex6/ex6/* machine-learning-ex6/
+git add .
+git commit -m "SVM snd Spam Classification"
+
+mkdir machine-learning-ex7
+cp /c/Users/cool_/Desktop/machine-learning-ex7/machine-learning-ex7/ex7/* machine-learning-ex7/
+git add .
+git commit -m "K-means and PCA"
+
+mkdir machine-learning-ex8
+cp /c/Users/cool_/Desktop/machine-learning-ex8/machine-learning-ex8/ex8/* machine-learning-ex8/
+git add .
+git commit -m "Anomaly detection and Recommender Systems"
+
